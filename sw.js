93ce44be79a4a71fb35f4d898527ea49a71f48ef
vwd99bqkt6-cmd <vwd99bqkt6@privaterelay.appleid.com>
2026-05-03 00:41:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+const CACHE = "hitster-v1";
+
 self.addEventListener("install", e => {
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "/Hitster-diy/",
+        "/Hitster-diy/index.html",
+        "/Hitster-diy/icon.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
